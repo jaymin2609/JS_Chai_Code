9272b9117bc12d7c4111d05b071fbe6bd3171da6
Jaymin Patel <37065167+jaymin2609@users.noreply.github.com>
2024-08-27 13:21:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,39 @@
+// let score = 32
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = true
+let score = "undefined"
+// let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let scoreNum = Number(score)
+
+console.log(typeof(scoreNum));
+
+console.log(scoreNum);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1
+// let isLoggedIn = "abc"
+// let isLoggedIn = ""
+
+let isLoggedInBool = Boolean(isLoggedIn)
+
+console.log(typeof(isLoggedInBool));
+console.log(isLoggedInBool);
+
+
+let someNum = 33
+let someNumStr = String(someNum)
+console.log(typeof(someNumStr));
+console.log(someNumStr);
+
+
+
+
