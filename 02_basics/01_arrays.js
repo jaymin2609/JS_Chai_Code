f9eaf62716c181ef6270856ce5c0974b7a4b5341
Jaymin Patel <37065167+jaymin2609@users.noreply.github.com>
2024-08-28 11:36:39 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// Array
+const myArray = [0,1,2,3,4,5,6,7,8]
+const myHeros = ["shaktiman","nagraj"]
+
+const myArr2 = new Array(1,4,7)
+console.log(myArray[3]);
+
+// Array Methods
+
+// myArray.push(98)
+// myArray.push(13)
+// myArray.pop()
+
+// myArray.unshift(44)
+// myArray.shift()
+
+// console.log(myArray.includes(3));
+// console.log(myArray.indexOf(3));
+
+// const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);
+
+// Slice, splice
+
+console.log("A ",myArray);
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+
+console.log("B ",myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+
+console.log("C ",myArray);
+
+
