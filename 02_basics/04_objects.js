c593c6fc9c364814102dbcdea6493d5c7e91676c
Jaymin Patel <37065167+jaymin2609@users.noreply.github.com>
2024-08-28 13:11:09 +0000

    Objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,7 +28,7 @@ const obj4 = {5:"a",6:"b"}
 // const obj3 = {obj1, obj2}
 // const obj3 = Object.assign({},obj1,obj2,obj4)
 const obj3 = {...obj1,...obj2,...obj4}
-console.log(obj3);
+// console.log(obj3);
 
 const users = [
     {
@@ -47,11 +47,28 @@ const users = [
 
 users[1].email
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('isLog'));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLog'));
+
+const course ={
+    courseName:"js in hindi",
+    price:"999",
+    instructor:"hitesh",
+
+}
+
+const {instructor:ins} = course
+
+console.log(ins);
+
+// {
+//     "name":"jaymin",
+
+//     "courseName":"JS in Hindi",
+// }
 
 
 
