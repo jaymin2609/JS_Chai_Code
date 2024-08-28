96398a3f0ed2c55225424eef8d5bc993e5e95301
Jaymin Patel <37065167+jaymin2609@users.noreply.github.com>
2024-08-28 08:21:09 +0000

    operations

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -6,14 +6,14 @@
 let score = "undefined"
 // let score = undefined
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let scoreNum = Number(score)
 
-console.log(typeof(scoreNum));
+// console.log(typeof(scoreNum));
 
-console.log(scoreNum);
+// console.log(scoreNum);
 
 // "33" => 33
 // "33abc" => NaN
@@ -25,14 +25,61 @@ let isLoggedIn = 1
 
 let isLoggedInBool = Boolean(isLoggedIn)
 
-console.log(typeof(isLoggedInBool));
-console.log(isLoggedInBool);
+// console.log(typeof(isLoggedInBool));
+// console.log(isLoggedInBool);
 
 
 let someNum = 33
 let someNumStr = String(someNum)
-console.log(typeof(someNumStr));
-console.log(someNumStr);
+// console.log(typeof(someNumStr));
+// console.log(someNumStr);
+
+
+// OPERATIONS
+
+ let value =3 
+ let negVal = -value
+
+//  console.log(negVal);
+
+//  console.log(2+2);
+
+//  console.log(2-2);
+
+//  console.log(2*2);
+
+//  console.log(2**2);
+
+//  console.log(2/2);
+ console.log(2%2);
+
+ let str1 = "Jaymin"
+ let str2 = "Hello"
+
+ let str3 = str2 + str1
+//  console.log(str3);
+ 
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log("1"+2+2);
+//  console.log(1+2+"2");
+//  console.log((3+4)*5%3);
+
+ console.log(+true);
+ console.log(+"");
+
+ let num1, num2, num3
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 
