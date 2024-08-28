f81df532cc8fb9e488c31c25649b81382980dffc
Jaymin Patel <37065167+jaymin2609@users.noreply.github.com>
2024-08-28 08:54:39 +0000

    data type summary

---
+++ 01_basics/data_types_summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, Null, Bigint, Undefined, Symbol
+
+const score = 100
+const scoreVal = 100.3
+
+const isLoggedIn = true
+const outTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber = 24142343523523523525n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","hanuman","doga","krish"];
+
+
+let myObj = {
+    name:"Abc",
+    age:32
+}
+
+const myFun = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof bigNumber);
+
+
