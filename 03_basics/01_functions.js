dc19f4e19157fd7171b51dee46f64b9a6eb11bf2
Jaymin Patel <37065167+jaymin2609@users.noreply.github.com>
2024-08-29 06:28:17 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,6 +38,43 @@ function loginUserMessage(userName="sam"){
 }
 
 // console.log(loginUserMessage("hitesh"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1;
+
+}
+
+console.log(calculateCartPrice(3,4,5,100,200));
+
+const user = {
+    userName:"jaymin",
+    price:100
+}
+
+function handleObject(anyObject){
+
+    console.log(`username is ${anyObject.userName} and price is ${anyObject.price}`);
+    
+}
+
+// console.log(handleObject(user));
+console.log(handleObject({
+    userName:"sam",
+    price:200
+}));
+
+const myNewArr = [200,400,100,600]
+
+function returnSecondVal(getArry){
+    
+return getArry[1]
+}
+
+// console.log(returnSecondVal(myNewArr));
+console.log(returnSecondVal([100,200,3,5,6]));
+
+
+
 
 
